7f2821919ddeb31f6cadc5b4741504f3393c979c
tkestowicz <tkestowicz@gmail.com>
2014-05-29 00:38:47 +0200

    Initial commit, prepared project and added add / remove item buttons.

---
+++ todo-burntdown/Scripts/main.ts
@@ -0,0 +1,6 @@
+﻿/// <reference path="typings/jquery/jquery.d.ts"/>
+import todo = require('viewmodels/todoViewModel');
+
+$(() => {
+    ko.applyBindings(new todo.TodoViewModel(), $('body')[0]);
+});
---
+++ todo-burntdown/Scripts/viewmodels/todoViewModel.ts
@@ -0,0 +1,34 @@
+﻿/// <reference path="../typings/knockout/knockout.d.ts"/>
+
+export interface ITodoItem {
+    id: number;
+    isDone: boolean;
+    text: string;
+    expectedWork: number;
+    actualWork: number;
+}
+
+export class TodoViewModel {
+    
+    todoItems: KnockoutObservableArray<ITodoItem> = ko.observableArray([]);
+
+    constructor() {
+        
+    }
+
+    newTodoItem = () => {
+        this.todoItems.push({
+            id: this.todoItems().length,
+            isDone: false,
+            text: 'Treść zadania',
+            expectedWork: 0,
+            actualWork: 0
+        });
+    }
+
+    removeTodoItem = (item) => {
+        this.todoItems.remove(it => it.id === item.id);
+        console.log(arguments);
+    }
+
+};
