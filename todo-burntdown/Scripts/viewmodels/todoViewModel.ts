38bc49c1b834c65b3d9eadaa6217425bb937c476
tkestowicz <tkestowicz@gmail.com>
2014-05-29 02:06:20 +0200

    Added basic work summary.

--- todo-burntdown/Scripts/viewmodels/todoViewModel.ts
+++ todo-burntdown/Scripts/viewmodels/todoViewModel.ts
@@ -2,33 +2,54 @@
 
 export interface ITodoItem {
     id: number;
-    isDone: boolean;
-    text: string;
-    expectedWork: number;
-    actualWork: number;
+    isDone: KnockoutObservable<boolean>;
+    text: KnockoutObservable<string>;
+    expectedWork: KnockoutObservable<number>;
+    actualWork: KnockoutObservable<number>;
 }
 
+export interface IWorkSummary {
+    expected: KnockoutComputed<number>;
+    actual: KnockoutComputed<number>;
+}
+
+var sumElements = (sum: number, element: number, index: number, array: number[]): number => {
+    return sum + parseInt(element.toString());
+};
+
 export class TodoViewModel {
-    
+
     todoItems: KnockoutObservableArray<ITodoItem> = ko.observableArray([]);
 
+    calculateSum = <TElement>(items: TElement[], item: (item: TElement) => number) => {
+        var result = items
+            .map(item)
+            .reduce(sumElements, 0);
+
+        return isNaN(result) ? 0 : result;        
+    };
+
+    workSummary: IWorkSummary = {
+        expected: ko.computed(() => this.calculateSum(this.todoItems(), (el) => el.expectedWork()), this),
+        actual: ko.computed(() => this.calculateSum(this.todoItems(), (el) => el.actualWork()), this)
+    };
+
     constructor() {
-        
+
     }
 
     newTodoItem = () => {
         this.todoItems.push({
             id: this.todoItems().length,
-            isDone: false,
-            text: 'Treść zadania',
-            expectedWork: 0,
-            actualWork: 0
+            isDone: ko.observable(false),
+            text: ko.observable(''),
+            expectedWork: ko.observable(0),
+            actualWork: ko.observable(0)
         });
     }
 
     removeTodoItem = (item) => {
         this.todoItems.remove(it => it.id === item.id);
-        console.log(arguments);
     }
 
 };
